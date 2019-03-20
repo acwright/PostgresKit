e236094b473b438a0d649176dc35d919184da541
Aaron Wright <aaron.c.wright@icloud.com>
2019-03-20 01:34:48 -0500

    Initial Commit

---
+++ PostgresKit/PostgresKit.h
@@ -0,0 +1,19 @@
+//
+//  PostgresKit.h
+//  PostgresKit
+//
+//  Created by Aaron Wright on 3/20/19.
+//  Copyright © 2019 Infinite Token. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PostgresKit.
+FOUNDATION_EXPORT double PostgresKitVersionNumber;
+
+//! Project version string for PostgresKit.
+FOUNDATION_EXPORT const unsigned char PostgresKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PostgresKit/PublicHeader.h>
+
+
